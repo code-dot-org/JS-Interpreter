2399c4f2a1590c47627082b0bcdafe110bc8ed7b
Chris Pirich <chris@code.org>
2019-12-13 11:52:10 -0800

    remove recursion while stepping through polyfill code

--- interpreter.js
+++ interpreter.js
@@ -188,22 +188,24 @@ Interpreter.prototype.appendCode = function(code) {
  * @return {boolean} True if a step was executed, false if no more instructions.
  */
 Interpreter.prototype.step = function() {
-  var stack = this.stateStack;
-  var state = stack[stack.length - 1];
-  if (!state) {
-    return false;
-  }
-  var node = state.node, type = node['type'];
-  if (type === 'Program' && state.done) {
-    return false;
-  } else if (this.paused_) {
-    return true;
-  }
-  this.functionMap_[type]();
-  if (!node['end']) {
-    // This is polyfill code.  Keep executing until we arrive at user code.
-    return this.step();
-  }
+  var node;
+  do {
+    var stack = this.stateStack;
+    var state = stack[stack.length - 1];
+    if (!state) {
+      return false;
+    }
+    var node = state.node, type = node['type'];
+    if (type === 'Program' && state.done) {
+      return false;
+    } else if (this.paused_) {
+      return true;
+    }
+    this.functionMap_[type]();
+    // When node['end'] is missing, we are in polyfill code.
+    // Keep executing until we arrive at user code.
+  } while (!node['end']);
+
   return true;
 };
 
